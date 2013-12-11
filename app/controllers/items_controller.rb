e57fa657b8d5c267eb0abee0a95e7c644bff5797
jakedavies <jgdavies24@gmail.com>
2013-12-10 21:37:57 -0800

    fixed colored circles

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,11 +7,11 @@ class ItemsController < ApplicationController
 	def index
 		@filter_message= params[:search]
 		@items = Item.all
-		if(params[:search]!=nil&&params[:search]!="")
+		if params[:search]!=nil&&params[:search]!=""
 			@items = Item.order('created_at DESC').find_all_by_tag(params[:search])
 
 		else
-			@items = Item.order("created_at DESC")
+			@items = Item.order('created_at DESC')
 		end
 	end
 
@@ -30,11 +30,11 @@ class ItemsController < ApplicationController
 	def format_timesince(time)
 
 		difference = (Time.zone.now- time)
-		if(difference<120)
+		if difference<120
 			return "about a minute ago"
-		elsif(difference<3600)
+		elsif difference<3600
 			return (difference/60).to_i.to_s + " minutes ago"
-		elsif(difference<86400)
+		elsif difference<86400
 			return (difference/3600).to_i.to_s + " hours ago"
 		else
 			return (difference/86400).to_i.to_s + " days ago"
