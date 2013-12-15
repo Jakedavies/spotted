b04074b6556b1ec6cf94f7b08fc5bc247b8b1044
jakedavies <jgdavies24@gmail.com>
2013-12-15 13:21:54 -0800

    changed line spacing in main files

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,45 +1,45 @@
 class ItemsController < ApplicationController
-	helper_method :format_timesince
+  helper_method :format_timesince
   helper_method :can_delete
-	def show
-		id = params[:id]
-		@item = Item.find(id)
-	end
-	def index
-		@filter_message= params[:search]
-		@items = Item.all
-		if params[:search]!=nil&&params[:search]!=""
-			@items = Item.order('created_at DESC').find_all_by_tag(params[:search])
-		else
-			@items = Item.order('created_at DESC')
-		end
-	end
+  def show
+    id = params[:id]
+    @item = Item.find(id)
+  end
+  def index
+    @filter_message= params[:search]
+    @items = Item.all
+    if params[:search]!=nil&&params[:search]!=""
+      @items = Item.order('created_at DESC').find_all_by_tag(params[:search])
+    else
+      @items = Item.order('created_at DESC')
+    end
+  end
 
-	def create
-      params[:item][:user] = session[:user_id]
-    	@item = Item.create!(params[:item])
-    	flash[:notice] = "Post Created"
-    	redirect_to items_path
-  	end
-	def destroy(id = params[:id])
-		@item = Item.find(id)
-		@item.destroy
-		redirect_to items_path
+  def create
+    params[:item][:user] = session[:user_id]
+    @item = Item.create!(params[:item])
+    flash[:notice] = "Post Created"
+    redirect_to items_path
+  end
+  def destroy(id = params[:id])
+    @item = Item.find(id)
+    @item.destroy
+    redirect_to items_path
   end
 
   #this is a helper method to format the time since post creation
-	def format_timesince(time)
+  def format_timesince(time)
 
-		difference = (Time.zone.now- time)
-		if difference<120
-			return "about a minute ago"
-		elsif difference<3600
-			return (difference/60).to_i.to_s + " minutes ago"
-		elsif difference<86400
-			return (difference/3600).to_i.to_s + " hours ago"
-		else
-			return (difference/86400).to_i.to_s + " days ago"
-		end
+    difference = (Time.zone.now- time)
+    if difference<120
+      return "about a minute ago"
+    elsif difference<3600
+      return (difference/60).to_i.to_s + " minutes ago"
+    elsif difference<86400
+      return (difference/3600).to_i.to_s + " hours ago"
+    else
+      return (difference/86400).to_i.to_s + " days ago"
+    end
   end
 
   #this is a helper method to determine if the post can be deleted
