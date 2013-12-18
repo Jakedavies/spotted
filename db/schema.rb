f477ac0116b7fa62c263a5216ca54520ac48c689
Nolan Koriath <nkoriath@shaw.ca>
2013-12-17 21:59:28 -0800

    Played With Padding

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(:version => 20131217035911) do
     t.text     "tag"
     t.string   "user"
     t.decimal  "views"
-    t.decimal  "votes"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
