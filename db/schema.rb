ece421883c6eeaaad5646b7970a30ccdd48fe1a9
jake <jgdavies24@gmail.com>
2014-01-03 12:11:44 -0800

    spotted logo now links to index

--- db/schema.rb
+++ db/schema.rb
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(:version => 20131217035911) do
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
   end
-
   add_index "votes", ["votable_id", "votable_type", "vote_scope"], :name => "index_votes_on_votable_id_and_votable_type_and_vote_scope"
   add_index "votes", ["votable_id", "votable_type"], :name => "index_votes_on_votable_id_and_votable_type"
   add_index "votes", ["voter_id", "voter_type", "vote_scope"], :name => "index_votes_on_voter_id_and_voter_type_and_vote_scope"
