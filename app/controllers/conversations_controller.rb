ba2373ea4d2873324971c2afc3cac9364c5853a3
jake <jgdavies24@gmail.com>
2014-01-20 14:26:27 -0800

    messages working

---
+++ app/controllers/conversations_controller.rb
@@ -0,0 +1,10 @@
+class ConversationsController < ApplicationController
+  helper_method :mailbox, :conversation
+  def mailbox
+    @mailbox ||= current_user.mailbox
+  end
+
+  def conversation
+    @conversation ||= mailbox.conversations.find(params[:id])
+  end
+end
